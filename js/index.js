237a3cb2cf189b2ba44ec241f12edce7f341794f
danitorres <publicomltda@gmail.com>
2024-02-07 09:04:58 -0300

    Vinculamos Git + GitHub

---
+++ js/index.js
@@ -0,0 +1,102 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.scroll1', {
+        duration: 4500,
+        origin: 'top',
+        distance: '-200px'
+    });
+    sr.reveal('.scroll2', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-200px'
+    });
+    sr.reveal('.scroll3', {
+        duration: 3500,
+        origin: 'top',
+        distance: '-200px'
+    });
+    sr.reveal('.scroll4', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-200px'
+    });
+    sr.reveal('.scroll5', {
+        duration: 2500,
+        origin: 'top',
+        distance: '-200px'
+    });
+    sr.reveal('.scroll6', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-200px'
+    });
+    sr.reveal('.font-up', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+    sr.reveal('.font-top', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-50px'
+    });
+    sr.reveal('.font-left', {
+        duration: 2000,
+        origin: 'left',
+        distance: '100px'
+    });
+    sr.reveal('.font-right', {
+        duration: 2000,
+        origin: 'right',
+        distance: '100px'
+    });
+    sr.reveal('.row-right', {
+        duration: 4000,
+        origin: 'right',
+        distance: '200px'
+    });
+    sr.reveal('.row-left', {
+        duration: 4000,
+        origin: 'left',
+        distance: '200px'
+    });
+    sr.reveal('.card1', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-200px'
+    });
+    sr.reveal('.card2', {
+        duration: 3500,
+        origin: 'top',
+        distance: '-150px'
+    });
+    sr.reveal('.card3', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-100px'
+    });
+    sr.reveal('.card4', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-50px'
+    });
+    sr.reveal('.team1', {
+        duration: 3500,
+        origin: 'top',
+        distance: '-50px'
+    });
+    sr.reveal('.team2', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-50px'
+    });
+    sr.reveal('.team3', {
+        duration: 4500,
+        origin: 'top',
+        distance: '-50px'
+    });
+    sr.reveal('.team4', {
+        duration: 4500,
+        origin: 'top',
+        distance: '-50px'
+    });
